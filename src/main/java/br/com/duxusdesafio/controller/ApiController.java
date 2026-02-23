13563cd25685690de4709705367885faca8c2de6
VcsDevs <v_chs@hotmail.com>
2026-02-23 18:17:48 -0300

    Update Controller

--- src/main/java/br/com/duxusdesafio/controller/ApiController.java
+++ src/main/java/br/com/duxusdesafio/controller/ApiController.java
@@ -6,6 +6,7 @@ import br.com.duxusdesafio.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +26,9 @@ public class ApiController {
         return apiService.adicionarTime(time.getData(), time.getComposicaoTime());
     }
 
-    @DeleteMapping("/deletar-time")
-    public void deletarTime(@RequestBody Time time) {
-        apiService.deletarTime(time);
+    @DeleteMapping("/deletar-time/{id}")
+    public void deletarTime(@PathVariable Long id) {
+        apiService.deletarTime(id);
     }
 
     @GetMapping("/listar-times")
@@ -35,18 +36,23 @@ public class ApiController {
         return apiService.listarTimes();
     }
 
-    @PostMapping("/adicionar-integrante")
-    public Integrante adicionarIntegrante(@RequestBody Integrante integrante){
-        return apiService.adicionarIntegrante(integrante.getFranquia(), integrante.getNome(), integrante.getFuncao());
+    @GetMapping("/listar-times/{id}")
+    public Time listarTimes(@PathVariable Long id){
+        return apiService.listarTimePeloId(id);
     }
 
-    @DeleteMapping("/deletar-integrante")
-    public void deletarIntegrante(@RequestBody Integrante integrante){
-        apiService.deletarIntegrante(integrante);
+    @PostMapping("/adicionar-integrante")
+    public Integrante adicionarIntegrante(@RequestBody Integrante integrante){
+        return apiService.adicionarIntegrante(integrante);
     }
 
     @GetMapping("/listar-integrantes")
     public List<Integrante> listarIntegrantes(){
         return apiService.listarIntegrantes();
+    }
+
+    @GetMapping("/listar-integrantes/{id}")
+    public Integrante listarIntegrantes(@PathVariable Long id){
+        return apiService.listarIntegrantePeloId(id);
     }
 }
